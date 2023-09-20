1d478b73ba38a7659b76a5fff831f910d4061be0
jabir254 <jabirkullow@gmail.com>
2023-09-20 11:27:10 +0300

    redirect  to homepage

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { addDoc, collection } from "firebase/firestore";
 import { auth, db } from "../../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 interface CreateFormData {
   title: string;
@@ -11,6 +12,7 @@ interface CreateFormData {
 }
 export const CreateForm = () => {
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   const schema = yup.object().shape({
     title: yup.string().required("You Must add a title"),
@@ -33,6 +35,8 @@ export const CreateForm = () => {
       username: user?.displayName,
       userId: user?.uid,
     });
+
+    navigate("/");
   };
   return (
     <form onSubmit={handleSubmit(onCreatePost)}>
