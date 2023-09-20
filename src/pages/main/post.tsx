26195784bd80656c44271550e1215e7c9c390b74
jabir254 <jabirkullow@gmail.com>
2023-09-20 21:38:55 +0300

    added remove like function

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,11 +1,73 @@
 import { Post as IPost } from "./main";
+import { addDoc, collection, query, where, getDocs, deleteDoc} from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
 interface Props {
   post: IPost;
 }
+interface Like {
+  userId: string;
+}
+
 export const Post = (props: Props) => {
   const { post } = props;
+
+  const [user] = useAuthState(auth);
+
+  const [likes, setLike] = useState<Like[] | null>(null);
+
+  const likeRef = collection(db, "likes");
+
+  const likesDoc = query(likeRef, where("postId", "==", post.id));
+
+  const getLikes = async () => {
+    const data = await getDocs(likesDoc);
+    setLike(data.docs.map((doc) => ({ userId: doc.data().userId })));
+  };
+
+  useEffect(() => {
+    getLikes();
+  }, []);
+
+  const addLike = async () => {
+    try {
+      await addDoc(likeRef, {
+        userId: user?.uid,
+        postId: post.id,
+      });
+      if (user) {
+        setLike((prev) =>
+          prev ? [...prev, { userId: user?.uid }] : [{ userId: user?.uid }]
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const removeLike = async () => {
+    try {
+      const likeToDelete = doc(db, "likes", )
+      await addDoc(likeRef, {
+        userId: user?.uid,
+        postId: post.id,
+      });
+      if (user) {
+        setLike((prev) =>
+          prev ? [...prev, { userId: user?.uid }] : [{ userId: user?.uid }]
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+  const hasUserLike = likes?.find((like) => like.userId === user?.uid);
+
   return (
-    <div>
+    <div className="mainbox">
       <div className="title">
         <h1>{post.title}</h1>
       </div>
@@ -14,6 +76,11 @@ export const Post = (props: Props) => {
       </div>
       <div className="footer">
         <p> @{post.username}</p>
+        <button onClick={addLike}>
+          {" "}
+          {hasUserLike ? <>&#128078;</> : <>&#128077;</>}{" "}
+        </button>
+        {likes && <p>Likes: {likes?.length}</p>}
       </div>
     </div>
   );
